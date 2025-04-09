870a0114c738c649f6cfe4c04c7e40311abf7bd1
plexixx <byx1029@live.com>
2025-04-09 22:57:29 +0300

    Fix the pipeline workflow

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
+app.use('/health', (req, res) => {
+  res.send('ok')
+})
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
